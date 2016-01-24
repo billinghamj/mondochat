7e18b6481d61f53d42ea981888d6dcb996b6b2ef
James Billingham <james@billingham.net>
2016-01-24 13:48:34 +0000

    test

--- app/handlers/oauth-response.js
+++ app/handlers/oauth-response.js
@@ -1,10 +1,35 @@
+import MondoClient from 'mondo-bank';
+import {resolve as urlResolve} from 'url';
+
 export default async function (req, res) {
-	const auth = req.app.get('mondo-auth');
-	const url = req.app.get('auth-redirect-url');
-	const code = req.query.code;
+	const redis = req.app.get('redis');
+	const mondoAuth = req.app.get('mondo-auth');
+	const authCode = req.query.code;
+	const redirUrl = req.app.get('auth-redirect-url');
+	const appBaseUrl = req.app.get('app-base-url');
 
-	const authInfo = await auth.createToken(code, url);
-	const token = authInfo.access_token;
+	const authinfo = await mondoAuth.createToken(authCode, redirUrl);
+	const mUserId = authinfo.user_id;
+	const mToken = authinfo.access_token;
 
-	res.json(token);
+	await redis.hset('users', mUserId, mToken);
+
+	const accounts = await MondoClient.accounts(mToken);
+
+	if (!accounts || !accounts.length)
+		throw new Error('no Mondo accounts found');
+
+	const account = accounts[0];
+
+	await MondoClient.createFeedItem({
+		account_id: account.id,
+		type: 'basic',
+		url: urlResolve(appBaseUrl, `chats/${mUserId}`),
+		params: {
+			title: 'New chat created',
+			image_url: 'http://i.imgur.com/TaPHLlV.png',
+		},
+	}, mToken);
+
+	res.text('👍');
 }
