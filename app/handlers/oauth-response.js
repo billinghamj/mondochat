21279b4c0cfbd3ba600862c77933b437ff3716a5
James Billingham <james@billingham.net>
2016-01-24 11:22:55 +0000

    test

--- app/handlers/oauth-response.js
+++ app/handlers/oauth-response.js
@@ -1,6 +1,6 @@
 export default async function (req, res) {
-	const auth = req.get('mondo-auth');
-	const url = req.get('auth-redirect-url');
+	const auth = req.app.get('mondo-auth');
+	const url = req.app.get('auth-redirect-url');
 	const code = req.query.code;
 
 	const redir = auth.authorizationUrl(url);
