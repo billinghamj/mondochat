c151d909842c9772486919285b2feb5cd3cb85ed
James Billingham <james@billingham.net>
2016-01-24 13:57:20 +0000

    test

--- app/handlers/oauth-response.js
+++ app/handlers/oauth-response.js
@@ -12,17 +12,19 @@ export default async function (req, res) {
 	const mUserId = authinfo.user_id;
 	const mToken = authinfo.access_token;
 
-	await redis.hset('users', mUserId, mToken);
+	await redis.hset('mondo_tokens', mUserId, mToken);
 
-	const accounts = await MondoClient.accounts(mToken);
+	const accounts = (await MondoClient.accounts(mToken)).accounts;
 
 	if (!accounts || !accounts.length)
 		throw new Error('no Mondo accounts found');
 
-	const account = accounts[0];
+	const accountId = accounts[0].id;
+
+	await redis.hset('mondo_accounts', mUserId, accountId);
 
 	await MondoClient.createFeedItem({
-		account_id: account.id,
+		account_id: accountId,
 		type: 'basic',
 		url: urlResolve(appBaseUrl, `chats/${mUserId}`),
 		params: {
